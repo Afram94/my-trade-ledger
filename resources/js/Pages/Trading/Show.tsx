2cbde932154cb99511b6b3805d127133071a4a13
Afram94 <afram.h@hotmail.com>
2023-12-20 09:55:49 +0100

    Added summary stats to Show component like Total amount Profitable Trades, Losing Trades, Best Trade, Worst Trade

--- resources/js/Pages/Trading/Show.tsx
+++ resources/js/Pages/Trading/Show.tsx
@@ -13,8 +13,18 @@ const Show: React.FC<PageProps> = ({ auth }) => {
     };
 
     // Calculate individual trade profit/loss and total profit/loss
-    const feePercentage = 0.50; // Total fee percentage for round-trip trade
+    // Initialize total profit/loss across all trades to zero.
     let totalProfitLoss = 0;
+
+    // Initialize counters for the number of profitable and losing trades.
+    let profitableTrades = 0;
+    let losingTrades = 0;
+
+    // Initialize an object to track the best trade. It starts with the worst possible profitLoss value (-Infinity) and no trade (null).
+    let bestTrade: { profitLoss: number; trade: Trade | null } = { profitLoss: -Infinity, trade: null };
+
+    // Initialize an object to track the worst trade. It starts with the best possible profitLoss value (Infinity) and no trade (null).
+    let worstTrade: { profitLoss: number; trade: Trade | null } = { profitLoss: Infinity, trade: null };
     /* trades.forEach(trade => {
         const profitOrLoss = trade.initial_capital * (trade.percentage / 100);
         totalProfitLoss += profitOrLoss;
@@ -32,33 +42,77 @@ const Show: React.FC<PageProps> = ({ auth }) => {
         trade.calculatedProfitLoss = adjustedProfitLoss;
     }); */
 
+    // Iterate over each trade in the trades array.
     trades.forEach(trade => {
+        // Calculate the raw profit or loss for the trade by multiplying the initial capital by the percentage gain/loss.
         const profitOrLoss = trade.initial_capital * (trade.percentage / 100);
-    
-        // Adjusting profit/loss by subtracting the fee
+
+        // Calculate the fee amount for the trade.
         const feeAmount = trade.initial_capital * (trade.fee_percentage / 100);
+
+        // Adjust the profit or loss by subtracting the fee amount.
         const adjustedProfitLoss = profitOrLoss - feeAmount;
-    
+
+        // Accumulate the adjusted profit or loss to the total profit/loss.
         totalProfitLoss += adjustedProfitLoss;
+
+        // Store the calculated profit or loss in the trade object.
         trade.calculatedProfitLoss = adjustedProfitLoss;
+
+        // Update the count of profitable trades if the adjusted profit/loss is positive.
+        if (adjustedProfitLoss > 0) {
+            profitableTrades++;
+        }
+        // Update the count of losing trades if the adjusted profit/loss is negative.
+        else if (adjustedProfitLoss < 0) {
+            losingTrades++;
+        }
+
+        // Check and update the best trade if the current trade has a higher profit/loss than the current best.
+        if (adjustedProfitLoss > bestTrade.profitLoss) {
+            bestTrade = { profitLoss: adjustedProfitLoss, trade };
+        }
+        // Check and update the worst trade if the current trade has a lower profit/loss than the current worst.
+        if (adjustedProfitLoss < worstTrade.profitLoss) {
+            worstTrade = { profitLoss: adjustedProfitLoss, trade };
+        }
     });
 
     return (
         <div className="container mx-auto p-4">
 
-            <div className="my-4 flex justify-between text-2xl">
-                <div>
+            <div className="my-4 text-2xl grid grid-cols-1 md:grid-cols-6 gap-2">
+                <div className="flex items-center">
                     <CreateModal />
                 </div>
-                <div className='flex items-center'>
-                    <p className='mx-3 font-bold'>
-                        <strong className='text-gray-500'>Total Profit/Loss: </strong>
-                        <p className={`flex justify-center text-gray-100 rounded-xl ${totalProfitLoss > 0 ? 'bg-green-500 text-green-200' : 'bg-red-500 text-red-200'}`}>
-                            {formatNumber(totalProfitLoss) } kr
-                        </p>
+                <div className="bg-white shadow-lg rounded-xl p-4 flex flex-col justify-center items-center">
+                    <p className="font-bold text-gray-700 text-lg">Total Profit/Loss:</p>
+                    <p className={`text-lg ${totalProfitLoss > 0 ? 'text-green-500' : 'text-red-500'}`}>
+                        {formatNumber(totalProfitLoss)} kr
                     </p>
                 </div>
+
+                <div className="bg-white shadow-lg rounded-xl p-4 flex flex-col justify-center items-center">
+                    <p className="font-bold text-gray-700 text-lg">Profitable Trades:</p>
+                    <p className="text-lg text-green-500">{profitableTrades}</p>
+                </div>
+
+                <div className="bg-white shadow-lg rounded-xl p-4 flex flex-col justify-center items-center">
+                    <p className="font-bold text-gray-700 text-lg">Losing Trades:</p>
+                    <p className="text-lg text-orange-500">{losingTrades}</p>
+                </div>
+
+                <div className="bg-white shadow-lg rounded-xl p-4 flex flex-col justify-center items-center">
+                    <p className="font-bold text-gray-700 text-lg">Best Trade:</p>
+                    <p className="text-sm text-green-500 text-center">{bestTrade.trade ? `${bestTrade.trade.stock_name} with Profit/Loss: ${formatNumber(bestTrade.profitLoss)} kr` : 'N/A'}</p>
+                </div>
+                <div className="bg-white shadow-lg rounded-xl p-4 flex flex-col justify-center items-center">
+                    <p className="font-bold text-gray-700 text-lg">Worst Trade:</p>
+                    <p className="text-sm text-red-500 text-center">{worstTrade.trade ? `${worstTrade.trade.stock_name} with Profit/Loss: ${formatNumber(worstTrade.profitLoss)} kr` : 'N/A'}</p>
+                </div>
             </div>
+            
+            {/* </div> */}
 
             <table className="table-auto w-full text-left whitespace-no-wrap">
                 <thead>
